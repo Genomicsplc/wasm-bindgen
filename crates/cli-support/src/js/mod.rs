9a65b90ed797cad0b11dbddbe07fa5af8bd87378
Joshua C. Randall <jcrandall@alum.mit.edu>
2020-04-30 11:49:16 +0000

    add WASIPolyfill to imports.wasi_unstable and allow with --no-modules

--- crates/cli-support/src/js/mod.rs
+++ crates/cli-support/src/js/mod.rs
@@ -597,10 +597,13 @@ impl<'a> Context<'a> {
         for (i, extra) in extra_modules.iter().enumerate() {
             let imports = match &mut imports {
                 Some(list) => list,
-                None => bail!(
-                    "cannot import from modules (`{}`) with `--no-modules`",
-                    extra
-                ),
+                None => {
+                    continue;
+                    // bail!(
+                    //     "cannot import from modules (`{}`) with `--no-modules`",
+                    //     extra
+                    // );
+                },
             };
             imports.push_str(&format!("import * as __wbg_star{} from '{}';\n", i, extra));
             imports_init.push_str(&format!("imports['{}'] = __wbg_star{};\n", extra, i));
@@ -648,6 +651,7 @@ impl<'a> Context<'a> {
                 async function init(input{init_memory_arg}) {{
                     {default_module_path}
                     const imports = {{}};
+                    imports.wasi_unstable = WASIPolyfill;
                     {imports_init}
 
                     if (typeof input === 'string' || (typeof Request === 'function' && input instanceof Request) || (typeof URL === 'function' && input instanceof URL)) {{
